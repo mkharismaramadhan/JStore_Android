77423e029a942c38dce2bd85c581fbe90e567fbc
mkharismaramadhan <47825379+mkharismaramadhan@users.noreply.github.com>
2019-05-02 17:55:32 +0700

    modul 9

--- app/src/main/java/com/example/mkharismaramadhan/jstore_android_mkharismaramadhan/MenuRequest.java
+++ app/src/main/java/com/example/mkharismaramadhan/jstore_android_mkharismaramadhan/MenuRequest.java
@@ -6,15 +6,14 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginRequest extends StringRequest {
-    private static final String Regis_URL = "http://10.0.2.2:8080/logincust";
+public class MenuRequest extends StringRequest {
+    private static final String Regis_URL = "http://10.10.52.135:8080/items";
     private Map<String, String> params;
 
-    public LoginRequest(String email, String password, Response.Listener<String> listener){
-        super(Method.POST, Regis_URL, listener, null);
+    public MenuRequest(int id, Response.Listener<String> listener){
+        super(Method.GET, Regis_URL, listener, null);
         params = new HashMap<>();
-        params.put("email", email);
-        params.put("password", password);
+        params.put("id", Integer.toString(id));
     }
 
     @Override
